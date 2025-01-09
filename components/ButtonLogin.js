1ce848c4efdc76be0c45d35e4500af61fa964368
Taiguara <taiguara.moura@gmail.com>
2025-01-09 20:19:59 -0300

    new file:   app/api/auth/[...nextauth]/rout.js new file:   auth.js modified:   components/ButtonLogin.js modified:   package-lock.json modified:   package.json

--- components/ButtonLogin.js
+++ components/ButtonLogin.js
@@ -12,6 +12,12 @@ const ButtonLogin = ({ isLoggedIn, name, extraStyle }) => {
     );
   }
   return <button>Login</button>;
+
+  // 1. Create a /login page
+
+  // 2. Create a email/password form
+
+  // 3. Make a POST request to /api/login
 };
 
 export default ButtonLogin;
