fe68c2ed83aa07d535cc6cef032a7b9d9f454506
Taiguara <taiguara.moura@gmail.com>
2025-01-20 20:38:00 -0300

    new file:   app/dashboard/layout.js 	modified:   app/dashboard/page.js 	modified:   app/page.js 	modified:   auth.js 	modified:   components/ButtonLogin.js 	new file:   components/ButtonLogout.js

--- auth.js
+++ auth.js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import Resend from "next-auth/providers/resend";
+import Google from "next-auth/providers/google";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import clientPromise from "./libs/mongo";
 
@@ -9,6 +10,10 @@ const config = {
       apiKey: process.env.RESEND_KEY,
       from: "noreply@resend.ptassist.pro",
       name: "Email",
+    }),
+    Google({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
   adapter: MongoDBAdapter(clientPromise),
