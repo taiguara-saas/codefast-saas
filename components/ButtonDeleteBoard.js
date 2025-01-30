039f6ff707f65dc8265f6b8a1fd6be3643bb5087
Taiguara <taiguara.moura@gmail.com>
2025-01-30 10:25:12 -0300

    modified:   components/ButtonDeleteBoard.js

--- components/ButtonDeleteBoard.js
+++ components/ButtonDeleteBoard.js
@@ -19,7 +19,6 @@ const ButtonDeleteBoard = ({ boardId }) => {
         toast.success("Board deleted!");
 
         router.push("/dashboard");
-        router.refresh();
       }
     } catch (error) {
       const errorMessage =
